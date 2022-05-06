c9c0bf99f54b30a4c0575cca40630ba0a78b7bcf
Robe <47053081+robegetachew@users.noreply.github.com>
2022-05-07 00:16:53 +0300

    all

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -14,11 +14,5 @@ void errors(void);
 int _strlen(char *s);
 int is_digit(char *s);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-char *create_array(unsigned int size, char c);
-char *_strdup(char *str);
-char *str_concat(char *s1, char *s2);
-int **alloc_grid(int width, int height);
-void free_grid(int **grid, int height);
-char *argstostr(int ac, char **av); 
 
 #endif
